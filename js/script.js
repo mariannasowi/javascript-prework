03d1fd92b1e72ecca8c0ad801bc33f983d6845ae
Marianna Sowinska <marianna.sowi@gmail.com>
2020-05-17 19:08:29 +0200

    Change a new task

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+let displayResult = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('moves:', argComputerMove, argPlayerMove);
 
-let computerMove = getMoveName(randomNumber);
+let argComputerMove = getMoveName(randomNumber);
 
 if(randomNumber == '1'){
-  computerMove = 'kamień';
+  argComputerMove = 'kamień';
 }
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Mój ruch to: ' + argComputerMove);
 
 if(playerInput == '2'){
-  playerMove = 'papier';
+  argplayerMove = 'papier';
 }
-printMessage('Twój ruch to: ' + computerMove);
+printMessage('Twój ruch to: ' + argComputerMove);
 
 if(playerInput == '3'){
-  playerMove = 'nożyce';
+  argPlayerMove = 'nożyce';
 }
-printMessage('Twój ruch to: ' + computerMove);
+printMessage('Twój ruch to: ' + argComputerMove);
 
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -42,19 +42,19 @@ if(playerInput == '3'){
 printMessage('Twój ruch to: ' + computerMove);
 
 
-const p1 = (computerMove == 'kamień' && playerMove == 'papier');
-const p2 = (computerMove == 'papier' && playerMove == 'nożyce');
-const p3 = (computerMove == 'nożyce' && playerMove == 'kamień');
+const p1 = (argComputerMove == 'kamień' && argPlayerMove == 'papier');
+const p2 = (argComputerMove == 'papier' && argPlayerMove == 'nożyce');
+const p3 = (argComputerMove == 'nożyce' && argPlayerMove == 'kamień');
 
-const r1 = (computerMove == 'kamień' && playerMove == 'kamień');
-const r2 = (computerMove == 'papier' && playerMove == 'papier');
-const r3 = (computerMove == 'nożyce' && playerMove == 'nożyce');
+const r1 = (argComputerMove == 'kamień' && argPlayerMove == 'kamień');
+const r2 = (argComputerMove == 'papier' && argPlayerMove == 'papier');
+const r3 = (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce');
 
-const n1 = (computerMove == 'kamień' && playerMove == '0')
-const n2 = (computerMove == 'nożyce' && playerMove == '0')
-const n3 = (computerMove == 'papier' && playerMove == '0')
+const n1 = (argComputerMove == 'kamień' && argPlayerMove == '0')
+const n2 = (argComputerMove == 'nożyce' && argPlayerMove == '0')
+const n3 = (argComputerMove == 'papier' && argPlayerMove == '0')
 
-if(p1 || p2 || p3){
+if (p1 || p2 || p3){
   printMessage('Ty wygrywasz!');
 } else if (r1 || r2 || r3){
   printMessage('Remis!');
@@ -64,7 +64,7 @@ if(p1 || p2 || p3){
   printMessage('Nieznany ruch!');
 }
 
-function getMoveName(moveId){
+function displayResult (moveId){
   if (moveId == 2){
     return 'papier';
   } else if (moveId == 3){
