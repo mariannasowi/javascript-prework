47268b489deebaf3c21df5784e6d5d232625de8d
Marianna Sowinska <marianna.sowi@gmail.com>
2020-05-20 19:05:53 +0200

    Add task: Zadanie: Poprawa kodu projektu-1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function getMoveName(arg1) {
+{function getMoveName(arg1) {
   if (arg1 == 1) {
     return 'kamień';
   } else if (arg1 == 2) {
@@ -12,25 +12,25 @@ function getMoveName(arg1) {
 
 function playGame (playerInput){
   clearMessages();
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  let argComputerMove = getMoveName(randomNumber);
+  let randomNumber = Math.floor(Math.random() * 3 + 1),
+      argComputerMove = getMoveName(randomNumber),
   //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-  let argPlayerMove = getMoveName(playerInput);
+      argPlayerMove = getMoveName(playerInput);
 
   printMessage('Twój ruch to: ' + argPlayerMove);
   printMessage('Komputera ruch to: ' + argComputerMove);
 
-  const p1 = (argComputerMove == 'kamień' && argPlayerMove == 'papier');
-  const p2 = (argComputerMove == 'papier' && argPlayerMove == 'nożyce');
-  const p3 = (argComputerMove == 'nożyce' && argPlayerMove == 'kamień');
+  const p1 = (argComputerMove == 'kamień' && argPlayerMove == 'papier'),
+        p2 = (argComputerMove == 'papier' && argPlayerMove == 'nożyce'),
+        p3 = (argComputerMove == 'nożyce' && argPlayerMove == 'kamień');
 
-  const r1 = (argComputerMove == 'kamień' && argPlayerMove == 'kamień');
-  const r2 = (argComputerMove == 'papier' && argPlayerMove == 'papier');
-  const r3 = (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce');
+  const r1 = (argComputerMove == 'kamień' && argPlayerMove == 'kamień'),
+        r2 = (argComputerMove == 'papier' && argPlayerMove == 'papier'),
+        r3 = (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce');
 
-  const n1 = (argComputerMove == 'kamień' && argPlayerMove == '0');
-  const n2 = (argComputerMove == 'nożyce' && argPlayerMove == '0');
-  const n3 = (argComputerMove == 'papier' && argPlayerMove == '0');
+  const n1 = (argComputerMove == 'kamień' && argPlayerMove == '0'),
+        n2 = (argComputerMove == 'nożyce' && argPlayerMove == '0'),
+        n3 = (argComputerMove == 'papier' && argPlayerMove == '0');
 
   if(p1 || p2 || p3){
     printMessage('Ty wygrywasz!');
@@ -43,19 +43,18 @@ function playGame (playerInput){
   }
 }
 
-playGame(3);
-
-/*let playRockButton = document.getElementById ("play-rock");
+let playRockButton = document.getElementById ("play-rock");
 playRockButton.addEventListener('click', function(){
-  clearMessages('Guzik został kliknięty 1');
+  playGame('1');
 });
 
 let playPaperButton = document.getElementById ("play-paper");
 playPaperButton.addEventListener('click', function(){
-  clearMessages('Guzik został kliknięty 2');
+  playGame('2');
 });
 
 let playScissorsButton = document.getElementById ("play-scissors");
 playScissorsButton.addEventListener('click', function(){
-  clearMessages('Guzik został kliknięty 3');
-});*/
+  playGame('3');
+});
+}
